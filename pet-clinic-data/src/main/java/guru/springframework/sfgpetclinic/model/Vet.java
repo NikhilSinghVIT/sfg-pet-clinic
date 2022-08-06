4534cfbdda9364fd049480721174797c77313120
NikhilSinghVIT <nikhilsingh.ns2014@gmail.com>
2022-08-06 21:11:55 +0530

    added more info to the owner pojo

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -2,9 +2,7 @@ package guru.springframework.sfgpetclinic.model;
 
 import java.util.Set;
 
-/**
- * Created by jt on 7/13/18.
- */
+
 public class Vet extends Person {
     Set<Speciality> specialities;
 
