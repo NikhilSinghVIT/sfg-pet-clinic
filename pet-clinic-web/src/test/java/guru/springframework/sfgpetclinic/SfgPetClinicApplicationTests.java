87705e7f8f438fd46d72c01e9d40eedacc29ea59
NikhilSinghVIT <nikhilsingh.ns2014@gmail.com>
2022-08-06 17:24:33 +0530

    added the wro4j plugin

--- pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -7,8 +7,8 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {SfgPetClinicApplication.class})
 public class SfgPetClinicApplicationTests {
 
     @Test
